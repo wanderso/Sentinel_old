f82cf9ee4588264c530dfbb4929012df70a1c85d
willi <williamharrisanderson@gmail.com>
2019-05-05 22:15:51 -0600

    Some progress on world design and engine.

--- character.py
+++ character.py
@@ -69,6 +69,44 @@ class Minion(Character):
         return str(self.name)
 
 
+class Lieutenant(Character):
+    def __init__(self, name, die, context={}):
+        super().__init__(name, context=context)
+        self.name = name
+        self.max_die = die
+        self.value = None
+        self.current_die = Dice(self.max_die.get_die_size(), context=self.max_die.get_context())
+
+    def get_current_die(self):
+        return self.current_die
+
+    def roll_die(self):
+        self.value = self.current_die.roll()
+        return self.value
+
+    def take_damage(self, damage):
+        self.roll_die()
+        damage_save = self.value
+        if damage_save < damage:
+            if self.current_die.get_die_size() == 4:
+                self.remove()
+            else:
+                self.current_die.decrease_die_size()
+
+    def heal_damage(self, heal_count):
+        for _ in range (0, heal_count):
+            if self.current_die.get_die_size() < self.max_die.get_die_size():
+                self.current_die.increase_die_size()
+
+    def remove(self):
+        print("Removing %s" % self.name)
+        self.world.remove_entity(self)
+
+    def __str__(self):
+        return str(self.name)
+
+
+
 if __name__ == "__main__":
     d1 = Dice(6, context={'debug':True})
     d2 = Dice(8, context={'debug':True})
